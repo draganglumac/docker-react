bfd87c1ec34a79f24eb2f6a7d093e208db424b84
draganglumac <dragan.glumac@gmail.com>
2020-01-10 07:54:36 +0000

    Docker compose business.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ReactJS is the bee's knees 👍
         </a>
       </header>
     </div>
